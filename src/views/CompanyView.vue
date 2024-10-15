12dd946e977ece124681985cb5eafaa731a26f44
youthzt <84439336+youthzt@users.noreply.github.com>
2024-10-15 11:34:55 +0800

    完成企业文化第一个导航栏

--- src/views/CompanyView.vue
+++ src/views/CompanyView.vue
@@ -1,6 +1,70 @@
 <template>
     <div class="company-view">
-        <h1>企业文化页面</h1>
+        <div id="Content">
+            <div id="Content1">
+                <div>
+                    <div class="fgj">企业文化</div>
+                    <div class="about_nav">
+                        <ul>
+                            <li><span class="tu"><span><a href='/html1/category/181312/8528-1.htm' target='_self'
+                                            title=""><img src='../assets/2012680033.png'
+                                                alt='企业文化纲要' /></a></span></span><span class="tit"><span
+                                        id="CurrentlyNode">企业文化纲要</span></span></li>
+                            <li><span class="tu"><span><a href='/html1/category/181312/8530-1.htm' target='_self'
+                                            title=""><img src='../assets/2012680032.png'
+                                                alt='企业文化建设' /></a></span></span><span class="tit"><span id="NodeTitle"><a
+                                            href='/html1/category/181312/8530-1.htm' target='_self'
+                                            title="企业文化建设">企业文化建设</a></span></span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="Content3">
+                <div class="new_list">
+                    <ul>
+                        <li>
+                            <span id='ReportIDname'>
+                                <a href='/html1/report/2402/1167-1.htm' target='_blank'
+                                    title="中邮证券企业文化体系发布">中邮证券企业文化体系发布</a>
+                            </span>
+                            <span id='ReportIDIssueTime'>2024-02-02</span>
+                        </li>
+                        <li><span id='ReportIDname'><a href='/html1/report/2205/6885-1.htm' target='_blank'
+                                    title="“连接美好 无处不在”：中国邮政发布全新品牌定位体系">“连接美好 无处不在”：中国邮政发布全新品牌定位体系</a></span><span
+                                id='ReportIDIssueTime'>2022-05-10</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/8778-1.htm' target='_blank'
+                                    title="企业文化手册">企业文化手册</a></span><span id='ReportIDIssueTime'>2018-10-20</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9166-1.htm' target='_blank'
+                                    title="中国邮政企业文化 “一三五”企业文化体系概貌">中国邮政企业文化 “一三五”企业文化体系概貌</a></span><span
+                                id='ReportIDIssueTime'>2018-10-20</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9164-1.htm' target='_blank'
+                                    title="中国邮政企业文化理念识别系统内涵解读">中国邮政企业文化理念识别系统内涵解读</a></span><span
+                                id='ReportIDIssueTime'>2018-10-20</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9162-1.htm' target='_blank'
+                                    title="中国邮政企业文化行为识别系统内涵解读">中国邮政企业文化行为识别系统内涵解读</a></span><span
+                                id='ReportIDIssueTime'>2018-10-20</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9160-1.htm' target='_blank'
+                                    title="中国邮政企业文化企业使命的内涵诠释">中国邮政企业文化企业使命的内涵诠释</a></span><span
+                                id='ReportIDIssueTime'>2018-10-20</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9158-1.htm' target='_blank'
+                                    title="中国邮政企业文化邮政徽标的百年演变">中国邮政企业文化邮政徽标的百年演变</a></span><span
+                                id='ReportIDIssueTime'>2018-10-20</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9189-1.htm' target='_blank'
+                                    title="中国邮政企业文化理念识别系统应用规范">中国邮政企业文化理念识别系统应用规范</a></span><span
+                                id='ReportIDIssueTime'>2018-10-19</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/8730-1.htm' target='_blank'
+                                    title="中国邮政企业文化宣贯工作实施意见">中国邮政企业文化宣贯工作实施意见</a></span><span
+                                id='ReportIDIssueTime'>2018-10-19</span></li>
+                        <li><span id='ReportIDname'><a href='/html1/report/181312/9156-1.htm' target='_blank'
+                                    title="中国邮政集团公司党组关于开展企业文化示范点建设工作的实施意见">中国邮政集团公司党组关于开展企业文化示范点建设工作的实施意见</a></span><span
+                                id='ReportIDIssueTime'>2018-10-19</span></li>
+                    </ul>
+                </div>
+
+            </div>
+            <div id="Content2"></div>
+        </div>
+
     </div>
 </template>
 
@@ -9,23 +73,151 @@ export default {
     name: 'CompanyView',
     data() {
         return {
-            
+
         };
     },
     methods: {
-        
+
     },
     computed: {
-        
+
     },
     created() {
-        
+
     }
 };
 </script>
 
 <style scoped>
-.company-view {
-    /* Add your component styles here */
+#Content {
+    width: 1200px;
+    margin: auto;
+    overflow: hidden;
+}
+
+a {
+    text-decoration: none;
+    color: #040404;
+}
+
+body {
+    font-family: 'Microsoft YaHei', 'Helvetica', 'Arial';
+}
+
+#Content1 {
+    width: 282px;
+    float: left;
+}
+
+.fgj {
+    display: block;
+    font-size: 22px;
+    color: #2E2E30;
+    height: 62px;
+    line-height: 1;
+    padding-top: 25px;
+    position: relative;
+    padding-left: 15px;
+}
+
+.fgj::before {
+    content: "";
+    width: 8px;
+    height: 20px;
+    position: absolute;
+    left: 0px;
+    top: 26px;
+    background: #18AE66;
+    border-radius: 4px;
+}
+
+li {
+    list-style: none;
+}
+
+.about_nav ul li {
+    border-bottom: 1px solid #EAEAEE;
+    height: 70px;
+    overflow: hidden;
+    line-height: 70px;
+}
+
+ol,
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+.about_nav {
+    width: 280px;
+    margin-left: 2px;
+    background: #FFFFFF;
+    box-shadow: 0 2px 8px 0 rgba(2, 18, 40, 0.06);
+    border-radius: 4px;
+    margin-bottom: 100px;
+}
+
+.about_nav ul li .tu {
+    width: 40px;
+    padding: 0px 18px 0px 60px;
+    /* float: left; */
+    display: inline-block;
+}
+
+a {
+    text-decoration: none;
+    color: #040404;
+}
+
+img {
+    vertical-align: middle;
+}
+
+.about_nav ul li .tu img {
+    max-width: 100%;
+    margin: 0;
+}
+
+#Content3 {
+    width: 896px;
+    float: right;
+}
+
+.new_list {
+    margin-top: 106px;
+}
+
+.new_list li {
+    background: none;
+    position: relative;
+    padding-left: 20px;
+    margin-bottom: 21px;
+    font-size: 18px;
+    overflow: hidden;
+    zoom: 1;
+    height: 20px;
+    line-height: 20px;
+}
+
+.new_list li a {
+    color: #2E2E30;
+}
+
+.new_list li::before {
+    content: "";
+    background: #D8D8DC;
+    width: 4px;
+    height: 4px;
+    position: absolute;
+    top: 9px;
+    left: 5px;
+    border-radius: 4px;
+}
+
+.new_list li #ReportIDIssueTime {
+    float: right;
+    color: #8E8E92;
+    font-size: 12px;
 }
 </style>
